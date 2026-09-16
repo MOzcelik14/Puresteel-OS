d364adff8668fb989fe7a3379b81850f8674655d
Murat Özçelik <muratozclik@protonmail.com>
2026-09-16 23:26:56 +0300

    Migrate Puresteel desktop from KDE Plasma to Cinnamon

--- config/includes.chroot/etc/skel/.config/autostart-scripts/puresteel-wallpaper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-sleep 2
-plasma-apply-wallpaperimage /usr/share/backgrounds/puresteel/wallpaper.png >/dev/null 2>&1 || true
-rm -f "$HOME/.config/autostart-scripts/puresteel-wallpaper.sh"
